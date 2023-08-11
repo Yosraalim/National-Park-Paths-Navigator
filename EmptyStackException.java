d8d8797db844cb2106a15660adbf4f5e3edcd6ae
Yosraalim <124330510+Yosraalim@users.noreply.github.com>
2023-08-10 23:48:10 -0400

    Added the EmptyStackException java class file
    Added the EmptyStackException java class file

---
+++ EmptyStackException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ *
+ *  Represents the situation in which a stack is empty.
+ */
+
+public class EmptyStackException extends RuntimeException
+{
+  /**
+   * Sets up this exception with an appropriate message.
+   * @param message String representing the error encountered
+   */
+  public EmptyStackException (String message)
+  {
+    super (message);
+  }
+}
